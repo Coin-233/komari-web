e194ca52a29842655fed3f9e27d9c5ce71aae934
Coin-233 <53845501+Coin-233@users.noreply.github.com>
2025-07-28 07:27:55 +0000

    chore: 删两个按钮

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,9 @@
 import ThemeSwitch from "./ThemeSwitch";
-import ColorSwitch from "./ColorSwitch";
+// import ColorSwitch from "./ColorSwitch";
 import LanguageSwitch from "./Language";
 import LoginDialog from "./Login";
-import { IconButton } from "@radix-ui/themes";
-import { GitHubLogoIcon } from "@radix-ui/react-icons";
+// import { IconButton } from "@radix-ui/themes";
+// import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import { Link } from "react-router-dom";
 // import { usePublicInfo } from "@/contexts/PublicInfoContext";
 const NavBar = () => {
@@ -32,17 +32,17 @@ const NavBar = () => {
         </div> */}
       </div>
 
-      <IconButton
+      {/* <IconButton
         variant="soft"
         onClick={() => {
           window.open("https://github.com/komari-monitor", "_blank");
         }}
       >
         <GitHubLogoIcon />
-      </IconButton>
+      </IconButton> */}
 
       <ThemeSwitch />
-      <ColorSwitch />
+      {/* <ColorSwitch /> */}
       <LanguageSwitch />
       <LoginDialog />
     </nav>
