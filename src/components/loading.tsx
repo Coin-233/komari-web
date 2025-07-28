b603724081a46f0ff904acd6deb708bc7933cf58
Coin-233 <53845501+Coin-233@users.noreply.github.com>
2025-07-28 16:09:21 +0900

    style: 去掉加载图片
    为什么要这样做???

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -6,7 +6,7 @@ type LoadingProps = {
 const Loading = ({ text, children }: LoadingProps) => {
   return (
     <div className="flex items-center justify-center flex-col">
-        <img src="/assets/BlueArchive-loading.gif" className="h-16 mb-4" />
+{/*         <img src="/assets/BlueArchive-loading.gif" className="h-16 mb-4" /> */}
         <p className="text-lg font-bold">Loading...</p>
         <p className="text-sm text-muted-foreground mb-4">
             {text}
