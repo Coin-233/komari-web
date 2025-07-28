719de8cff7701e7ef991d5b6a9a6ec63f63e03d7
Coin-233 <53845501+Coin-233@users.noreply.github.com>
2025-07-28 07:17:49 +0000

    chore: 换文字

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -8,6 +8,7 @@ const Loading = ({ text, children }: LoadingProps) => {
     <div className="flex items-center justify-center flex-col">
 {/*         <img src="/assets/BlueArchive-loading.gif" className="h-16 mb-4" /> */}
         <p className="text-lg font-bold">Loading...</p>
+        <p className="text-base">不要心急</p>
         <p className="text-sm text-muted-foreground mb-4">
             {text}
         </p>
